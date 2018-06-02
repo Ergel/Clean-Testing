44d3aab776d0ff9a26ffaa275ac81df27897a4a1
Ergel <fuzuli_99@yahoo.com>
2018-06-02 23:42:52 +0200

    Moegliche Lösung der Aufgabe um nicht-Clean-Testing zu demonstrieren.

---
+++ Uebung-A-Beispiel/Giraffenpark.Business.AuthenticationServices/BenutzerAnmeldungService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using Giraffenpark.Business.DataServices;
+
+namespace Giraffenpark.Business.AuthenticationServices
+{
+    public class BenutzerAnmeldungService : IBenutzerAnmeldungService
+    {
+        public bool BenutzerRegistrieren(string benutzername, string vorname, string nachname, string passwort)
+        {
+            if (vorname.Equals("Özgür"))
+            {
+                return false;
+            }
+
+            RegisterBenutzer(benutzername, vorname, nachname, passwort);
+
+            return true;
+        }
+
+        private void RegisterBenutzer(string benutzername, string vorname, string nachname, string passwort)
+        {
+            var benutzer = new Benutzer();
+            benutzer.Benutzername = benutzername;
+            benutzer.Vorname = vorname;
+            benutzer.Nachname = "nachname";
+            benutzer.Passwort = passwort.Trim();
+
+            var model = new AuthenticationDatenModel();
+            model.Benutzer.Add(benutzer);
+            model.SaveChanges();
+        }
+
+        public bool DarfBenutzerAnmelden(string benutzername, string passwort)
+        {
+            var model = new AuthenticationDatenModel();
+            var benutzer = model.Benutzer.First(bntzer => bntzer.Benutzername == "benutzername");
+            var result = benutzer.Passwort.Trim().Equals(passwort);
+
+            return result;
+        }
+    }
+}
---
+++ Uebung-A-Beispiel/Giraffenpark.Test.Business.AuthenticationServices/BenutzerAnmeldungServiceTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Giraffenpark.Business.AuthenticationServices;
+using Giraffenpark.Business.DataServices;
+using NUnit.Framework;
+
+namespace Giraffenpark.Test.Business.AuthenticationServices
+{
+    [TestFixture]
+    public class BenutzerAnmeldungServiceTest
+    {
+        private static BenutzerAnmeldungService service;
+
+        [Test]
+        public void TesteDasInstanzierenDerServiceKlasse()
+        {
+            service = new BenutzerAnmeldungService();
+            Assert.That(service, Is.Not.Null);
+        }
+
+        [Test]
+        public void TesteDieRegisterBenutzerMethode()
+        {
+            var methodParameters = new object[]
+            {
+                "benutzername", "Özgür", "nachname", "passwort"
+            };
+
+            typeof(BenutzerAnmeldungService).GetMethod("RegisterBenutzer", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(new BenutzerAnmeldungService(), methodParameters); ;
+        }
+
+        [Test]
+        public void TesteSaveBenutzerInDatenbank()
+        {
+            var methodParameters = new object[]
+            {
+                "benutzername", "Özgür", "nachname", "passwort"
+            };
+
+            typeof(BenutzerAnmeldungService).GetMethod("RegisterBenutzer", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(new BenutzerAnmeldungService(), methodParameters); ;
+
+            var model = new AuthenticationDatenModel();
+            var benutzer = model.Benutzer.First(bntzer => bntzer.Benutzername == "benutzername");
+            Assert.That(benutzer.Benutzername, Is.Not.Empty);
+        }
+
+        [Test]
+        public void TesteMethodeDarfAnmelden()
+        {
+            var resultOfMethod = service.DarfBenutzerAnmelden("benutzername", "passwort");
+            Assert.That(resultOfMethod, Is.True);
+        }
+
+        [Test]
+        public void TesteÖzgürDarfNichtRegistrieren()
+        {
+            var resultOfMethod = service.BenutzerRegistrieren("Özgür", "Özgür", "nachname", "passwort");
+            Assert.That(resultOfMethod, Is.False);
+        }
+    }
+}
