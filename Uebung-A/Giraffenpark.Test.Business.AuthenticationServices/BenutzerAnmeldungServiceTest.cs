6ecfe4a1eb585f68d6986b909196ddb1865f4282
Ergel <fuzuli_99@yahoo.com>
2018-06-03 13:53:37 +0200

    Revert "Initiale Commit - Übung B-Beispiel für Clean Testing"
    This reverts commit c12445a5d0efbe9100ffa1d4002a67958b056410.

---
+++ Uebung-A/Giraffenpark.Test.Business.AuthenticationServices/BenutzerAnmeldungServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Giraffenpark.Business.AuthenticationServices;
+using Giraffenpark.Business.DataServices;
+using NUnit.Framework;
+
+namespace Giraffenpark.Test.Business.AuthenticationServices
+{
+    [TestFixture]
+    public class BenutzerAnmeldungServiceTest
+    {
+        [Test]
+        public void TesteDasInstanzierenDerServiceKlasse()
+        {
+            //todo: Befülle mich mit Leben.
+        }
+    }
+}
