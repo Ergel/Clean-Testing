380eeca4844a716ee6129346ce710064b8bd27af
Ergel <fuzuli_99@yahoo.com>
2018-06-03 23:57:53 +0200

    Initiale Commit - Vorlage für eine mögliche Lösung der Aufgabe D

--- Uebung-D/Giraffenpark.Business.AuthenticationServices/IBenutzerAnmeldungService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Giraffenpark.Business.AuthenticationServices
-{
-    public interface IBenutzerAnmeldungService
-    {
-        bool BenutzerRegistrieren(string benutzername, string vorname, string nachname, string passwort);
-        bool DarfBenutzerAnmelden(string benutzerName, string passwort);
-    }
-}
