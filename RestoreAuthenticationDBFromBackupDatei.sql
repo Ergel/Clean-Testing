8391275044b784510180066add9032e3f400190e
Özgür Ergel <fuzuli_99@yahoo.com>
2019-05-18 20:49:33 +0200

    Vorbereitung für Magdeburger Developer Days

--- RestoreAuthenticationDBFromBackupDatei.sql
+++ RestoreAuthenticationDBFromBackupDatei.sql
@@ -1,11 +1,11 @@
 USE [master]
-ALTER DATABASE [AuthenticationBase] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
-BACKUP LOG [AuthenticationBase] TO  
-DISK = N'C:\Entwicklung\Datenbanken\AuthenticationBase_LogBackup_2018-06-04_15-25-07.bak' WITH NOFORMAT, NOINIT, 
-NAME = N'AuthenticationBase_LogBackup_2018-06-04_15-25-07', NOSKIP, NOREWIND, NOUNLOAD,  NORECOVERY ,  STATS = 5
-RESTORE DATABASE [AuthenticationBase] FROM  DISK = N'C:\Entwicklung\Datenbanken\AuthenticationBase.bak' WITH  FILE = 1,  NOUNLOAD,  REPLACE,  STATS = 5
-ALTER DATABASE [AuthenticationBase] SET MULTI_USER
+RESTORE DATABASE [AuthenticationBase] 
+FROM  DISK = N'C:\Entwicklung\Datenbanken\AuthenticationBase.bak' 
+WITH  FILE = 1,  
+MOVE N'AuthenticationBase' 
+TO N'C:\Program Files\Microsoft SQL Server\MSSQL12.MSSQLSERVER\MSSQL\DATA\AuthenticationBase.mdf', 
+MOVE N'AuthenticationBase_log' 
+TO N'C:\Program Files\Microsoft SQL Server\MSSQL12.MSSQLSERVER\MSSQL\DATA\AuthenticationBase_log.ldf',  
+NOUNLOAD,  REPLACE,  STATS = 5
 
 GO
-
-
